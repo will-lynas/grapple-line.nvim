c6634fe58dc99ed92bd67b429476f153d235ecd6
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-07-07 19:22:10 +0100

    Add minimal version of plugin

--- lua/grapple-line.lua
+++ lua/grapple-line.lua
@@ -1,7 +1,26 @@
 local M = {}
 
 function M.status()
-	return "test123"
+	local grapple = require("grapple")
+	local result = {}
+	local current_index = grapple.name_or_index()
+
+	for i = 1, 4 do
+		if not grapple.exists({ index = i }) then
+			break
+		end
+		local file_info = grapple.find({ index = i })
+		local full_path = file_info.path
+		local filename = full_path:match("^.+/(.+)$")
+
+		if i == current_index then
+			table.insert(result, "%#lualine_a_normal# " .. filename .. " %*")
+		else
+			table.insert(result, "%#lualine_a_inactive# " .. filename .. " %*")
+		end
+	end
+
+	return table.concat(result)
 end
 
 return M
