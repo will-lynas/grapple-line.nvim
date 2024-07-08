8243313cae3fe84f66902283764324e18f82e457
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-07-08 10:06:32 +0100

    Refactor

--- lua/grapple-line.lua
+++ lua/grapple-line.lua
@@ -8,27 +8,45 @@ function M.setup(user_settings)
 	M.settings = vim.tbl_deep_extend("force", M.settings, user_settings)
 end
 
-function M.status()
+local function get_grapple_files()
 	local grapple = require("grapple")
-	local result = {}
+	local files = {}
 	local current_index = grapple.name_or_index()
 
 	for i = 1, M.settings.number_of_files do
 		if not grapple.exists({ index = i }) then
 			break
 		end
-		local file_info = grapple.find({ index = i })
-		local full_path = file_info.path
-		local filename = full_path:match("^.+/(.+)$")
-
-		if i == current_index then
-			table.insert(result, "%#lualine_a_normal# " .. filename .. " %*")
-		else
-			table.insert(result, "%#lualine_a_inactive# " .. filename .. " %*")
-		end
+		local path = grapple.find({ index = i }).path
+		local file = { path = path, current = i == current_index }
+		table.insert(files, file)
 	end
 
+	return files
+end
+
+local function format_paths(files)
+	for _, file in ipairs(files) do
+		file.path = file.path:match("^.+/(.+)$")
+	end
+	return files
+end
+
+local function make_statusline(files)
+	local result = {}
+	for _, file in ipairs(files) do
+		if file.current then
+			table.insert(result, "%#lualine_a_normal# " .. file.path .. " %*")
+		else
+			table.insert(result, "%#lualine_a_inactive# " .. file.path .. " %*")
+		end
+	end
 	return table.concat(result)
+end
+
+function M.status()
+	local files = format_paths(get_grapple_files())
+	return make_statusline(files)
 end
 
 return M
